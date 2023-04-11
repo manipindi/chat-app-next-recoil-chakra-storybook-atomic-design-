7e67e9345cf1bb0145bd33def0ce102d1f7c0874
manipindi <manikanta.pindi@narayanagroup.com>
2023-04-11 19:42:43 +0530

    scroll-triggered component added

--- framer-components/sub-items/scroll-examples/scroll-triggered/index.tsx
+++ framer-components/sub-items/scroll-examples/scroll-triggered/index.tsx
@@ -1,7 +1,77 @@
-import React from 'react'
+import { Box, Flex } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import React from "react";
+import styled from "styled-components";
+
+const Card = styled.div`
+  width: 90%;
+  background-color: gray;
+  height: 300px;
+  margin-top: auto;
+  z-index: 1;
+`;
+
+const PopupCard = styled(motion.div)`
+  width: 300px;
+  height: 400px;
+  position: absolute;
+  bottom: 250px;
+  z-index: -1;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: white;
+`;
 
 export default function ScrollTriggered() {
   return (
-    <div>ScrollTriggered</div>
-  )
+    <Flex
+      w={"100%"}
+      minH={"100vh"}
+      flexDir={"column"}
+      alignItems={"center"}
+      gap={"20px"}
+    >
+      <Flex
+        w={"90%"}
+        height={"80vh"}
+        justifyContent={"center"}
+        position={"relative"}
+      >
+        <Card />
+        <PopupCard  initial={{ opacity: 0, y:200 }} whileInView={{ opacity: 1, y:0 }}  transition={{duration:2}} viewport={{ once: true }}/>
+      </Flex>
+
+      <Flex
+        w={"90%"}
+        height={"80vh"}
+        justifyContent={"center"}
+        position={"relative"}
+      >
+        <Card />
+        <PopupCard  initial={{ opacity: 0, y:200 }} whileInView={{ opacity: 1, y:0 }}  transition={{duration:2}} viewport={{ once: true }}/>
+      </Flex>
+
+      <Flex
+        w={"90%"}
+        height={"80vh"}
+        justifyContent={"center"}
+        position={"relative"}
+      >
+        <Card />
+        <PopupCard initial={{ opacity: 0, y:200 }} whileInView={{ opacity: 1, y:0 }}  transition={{duration:2}} viewport={{ once: true }}/>
+      </Flex>
+
+      <Flex
+        w={"90%"}
+        height={"80vh"}
+        justifyContent={"center"}
+        position={"relative"}
+      >
+        <Card />
+        <PopupCard initial={{ opacity: 0, y:200 }} whileInView={{ opacity: 1, y:0 }} transition={{duration:2}} viewport={{ once: true }}/>
+      </Flex>
+    </Flex>
+  );
 }
--- pages/framer-motion/[sub-item]/index.tsx
+++ pages/framer-motion/[sub-item]/index.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import styled from "styled-components";
 
 export const DefaultWrapper = styled.div`
+ width: 100%;
   min-height: 100vh;
   display: flex;
   /* padding: 5rem; */
